210a73fb96800ac1066b63ee58d9817942cfc67b
Suzy Easton <suzyeaston@icloud.com>
2023-11-09 02:50:46 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,9 @@
+<?php
+function retro_game_music_theme_scripts() {
+    wp_enqueue_style( 'retro-font', 'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap' );
+    wp_enqueue_style( 'main-styles', get_stylesheet_uri() );
 
+    wp_enqueue_script( 'piano-script', get_template_directory_uri() . '/js/piano-script.js', array(), '1.0.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'retro_game_music_theme_scripts' );
