3c77ec574150ce89e60ee8ef17e515820ba3bbaf
Suzy Easton <suzyeaston@icloud.com>
2023-11-09 02:54:23 -0800

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <!-- Retro fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
