47457fae498250de26f982976905ceaacad71d33
Suzy Easton <suzyeaston@icloud.com>
2023-11-17 21:18:52 -0800

    Update single.php

--- single.php
+++ single.php
@@ -1,1 +1,36 @@
+<?php
+get_header();
+?>
 
+<main id="main-content">
+    <?php 
+    while ( have_posts() ) : the_post(); ?>
+    
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            </header>
+
+            <div class="entry-content">
+                <?php
+                the_content();
+                ?>
+            </div>
+
+            <footer class="entry-footer">
+                <?php
+                ?>
+            </footer>
+        </article>
+
+        <?php
+        if ( comments_open() || get_comments_number() ) :
+            comments_template();
+        endif;
+
+    endwhile; 
+    ?>
+</main>
+<?php
+get_footer();
+?>
