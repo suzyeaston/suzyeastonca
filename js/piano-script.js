9d079cc1d3ee30eddb97617bf5b3f87ce45badec
Suzy Easton <suzyeaston@icloud.com>
2023-11-17 21:04:05 -0800

    Create piano-script.js

---
+++ js/piano-script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const keys = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C2'];
+  const piano = document.getElementById('piano-container');
+
+  keys.forEach((key) => {
+    let pianoKey = document.createElement('div');
+    pianoKey.className = 'piano-key';
+    pianoKey.innerHTML = key;
+    pianoKey.dataset.note = key;
+    pianoKey.onclick = () => playNote(key);
+    piano.appendChild(pianoKey);
+  });
+
+  function playNote(key) {
+    let audio = new Audio(`.wav`);
+    audio.play();
+  }
+})
