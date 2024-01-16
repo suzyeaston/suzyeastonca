a4ef915c2e9c496d8ba48624e0ddfe33061ffbd3
Suzy Easton <suzyeaston@icloud.com>
2024-01-15 17:59:12 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?>
 
 <main id="main-content">
-    <section id="piano-container">
+    <section id="menu-container">
+        <div class="menu-item" onclick="location.href='link-to-social-media'">Social Media</div>
+        <div class="menu-item" onclick="location.href='link-to-career'">Career</div>
+        <div class="menu-item" onclick="location.href='link-to-music'">Music</div>
+        <div class="menu-item" onclick="location.href='link-to-bio'">Bio</div>
+        <div class="menu-item" onclick="location.href='link-to-game'">Suzy's Musical Adventure</div>
     </section>
 </main>
 
