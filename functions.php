8ba3e783c24d7ee9edec52b4a65fdbf3f809a423
Suzy Easton <suzy.easton@winedirect.com>
2024-02-26 22:15:03 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,9 +6,9 @@ function retro_game_music_theme_scripts() {
 
     wp_enqueue_style('main-styles', get_stylesheet_uri());
     
-    wp_enqueue_script('piano-script', get_template_directory_uri() . '/js/piano-script.js', array(), '1.0.1', true);
+    wp_enqueue_script('piano-script', get_template_directory_uri() . '/js/piano-script.js', array(), '1.0.1';
 
-    wp_enqueue_script('game-like-init', get_template_directory_uri() . '/js/game-like-init.js', array(), '1.0.1', true);
+    wp_enqueue_script('game-like-init', get_template_directory_uri() . '/js/game-like-init.js', array());
 }
 
 add_action('wp_enqueue_scripts', 'retro_game_music_theme_scripts');
