7a961f76873877f31be9399d915a41d9a9ff61c2
Suzy Easton <suzyeaston@icloud.com>
2024-03-13 00:00:16 -0700

    suzy picture

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <main id="main-content">
     <header id="retro-game-header">
         <div id="stacked-nerd-title">Stacked Nerd</div>
+        <img src="https://suzyeaston.ca/wp-content/uploads/2024/03/suzy2.jpeg" alt="Suzy Easton" style="max-width:100%; height:auto; display:block; margin:20px auto;">
     </header>
     <section id="menu-container">
         <div class="menu-item" onclick="location.href='https://twitter.com/officialsuzye'">Twitter</div>
