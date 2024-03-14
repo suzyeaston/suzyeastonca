1756d03042b0287ba34fc278545f0045deed0e05
Suzy Easton <suzyeaston@icloud.com>
2024-03-14 00:19:42 -0700

    setting up the template by copying over from page.php

--- page-contact.php
+++ page-contact.php
@@ -2,3 +2,20 @@
 /*
 Template Name: Contact Page
 */
+
+get_header();
+?>
+
+<main id="main-content">
+    <section class="page-content">
+        <?php
+        while ( have_posts() ) : the_post();
+            the_content();
+        endwhile;
+        ?>
+    </section>
+</main>
+
+<?php
+get_footer();
+?>
