b3016c876cec78aad3465b7ccc93b35681100303
Suzy Easton <suzyeaston@icloud.com>
2024-03-14 00:42:37 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,11 +10,6 @@ function retro_game_music_theme_scripts() {
     if (is_front_page()) {
         wp_enqueue_script('game-init', get_template_directory_uri() . '/js/game-init.js', array(), '1.0.0', true);
     }
-
-    if (is_page_template('page-contact.php')) {
-        wp_enqueue_style('contact-styles', get_template_directory_uri() . '/css/contact-styles.css', array(), '1.0.0');
-        wp_enqueue_script('contact-script', get_template_directory_uri() . '/js/contact-script.js', array(), '1.0.0', true);
-    }
 }
 
 add_action('wp_enqueue_scripts', 'retro_game_music_theme_scripts');
