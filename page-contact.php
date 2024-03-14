46be2e4217ea85b622d6c72bd8763ad27bacedd7
Suzy Easton <suzyeaston@icloud.com>
2024-03-14 00:32:16 -0700

    Update page-contact.php

--- page-contact.php
+++ page-contact.php
@@ -5,9 +5,12 @@ Template Name: Contact Page
 
 get_header();
 ?>
-
 <main id="main-content">
+    <header class="page-header">
+        <h1 class="glowing-header">Ways to Contact Suzy</h1>
+    </header>
     <section class="page-content">
+        <p>Email: <a href="mailto:info@suzyeaston.ca">info@suzyeaston.ca</a></p>
         <?php
         while ( have_posts() ) : the_post();
             the_content();
