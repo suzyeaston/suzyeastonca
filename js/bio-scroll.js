917334e35383d27d6555be51ed664b86d6978a73
Suzy Easton <suzyeaston@icloud.com>
2024-03-15 02:19:59 -0700

    change scrolling direction

--- js/bio-scroll.js
+++ js/bio-scroll.js
@@ -1,21 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const bioContainer = document.getElementById('bio-container');
     if (bioContainer) {
+        
         bioContainer.style.position = 'absolute';
-        bioContainer.style.bottom = '-100%';
-        window.scrollTo({ top: 0 });
+        bioContainer.style.whiteSpace = 'nowrap';
+        bioContainer.style.left = '-100%';
+        bioContainer.style.top = '0';
+
         animateBio();
     }
 
     function animateBio() {
-        let startPosition = window.innerHeight;
-        const endPosition = -bioContainer.offsetHeight;
+        let startLeft = -bioContainer.offsetWidth; // Start from the left
+        const endPosition = window.innerWidth;
 
         const interval = setInterval(() => {
-            startPosition -= 2;
-            if (startPosition <= endPosition) clearInterval(interval);
+            startLeft += 2;
+            if (startLeft >= endPosition) clearInterval(interval);
 
-            bioContainer.style.bottom = `${startPosition}px`;
-        }, 20);
+            bioContainer.style.left = `${startLeft}px`;
+        }, 20); // Adjust for smoother or faster animation
     }
 });
