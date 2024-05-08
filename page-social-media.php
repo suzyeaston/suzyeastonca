9ac38abe86bdb03ad23776fa1220009a55392969
Suzy Easton <suzyeaston@icloud.com>
2024-05-07 23:46:11 -0700

    Create page-social-media.php

---
+++ page-social-media.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Social Media Page
+*/
+
+get_header();
+?>
+<main id="main-content">
+    <header class="page-header">
+        <h1 class="glowing-header">Social Media</h1>
+    </header>
+    <section class="page-content">
+        <div class="menu-item" onclick="location.href='https://twitter.com/officialsuzye'">Twitter</div>
+        <div class="menu-item" onclick="location.href='https://www.linkedin.com/in/suzyeaston/'">Linkedin</div>
+        <div class="menu-item" onclick="location.href='https://suzyeaston.bandcamp.com/'">Music</div>
+        <div class="menu-item" onclick="location.href='https://www.youtube.com/user/anabsolutepitch'">YouTube</div>
+    </section>
+</main>
+<?php
+get_footer();
+?>
