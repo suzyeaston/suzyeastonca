2a461384a44f0210672c838c5246615c66724a5d
Suzy Easton <suzyeaston@icloud.com>
2024-05-08 00:48:04 -0700

    Update page-music-releases.php

--- page-music-releases.php
+++ page-music-releases.php
@@ -6,8 +6,8 @@ Template Name: Music Releases Page
 get_header();
 ?>
 <main id="main-content">
-    <header class="standard-header">
-        Music Releases
+    <header id="retro-game-header">
+        <div id="stacked-nerd-title">Music Releases</div>
     </header>
     <section class="page-content">
         <?php
