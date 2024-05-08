4438f735aa2112c246d63c9755c822ba2fef54a4
Suzy Easton <suzyeaston@icloud.com>
2024-05-08 00:36:21 -0700

    Update page-midnight-mix.php

--- page-midnight-mix.php
+++ page-midnight-mix.php
@@ -6,8 +6,8 @@ Template Name: Midnight Mix Page
 get_header();
 ?>
 <main id="main-content">
-    <header class="page-header">
-        <h1 class="glowing-header">Midnight Mix with Suzy Easton: Live Stream Music Sessions</h1>
+    <header class="standard-header">
+        Midnight Mix with Suzy Easton: Live Stream Music Sessions
     </header>
     <section class="page-content">
         <?php
