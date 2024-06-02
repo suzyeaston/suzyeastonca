ff1f69ca89543c05f8e54e238bdcd49de7db0cb5
Suzy Easton <suzyeaston@icloud.com>
2024-06-01 20:22:20 -0700

    Update page-bio.php

--- page-bio.php
+++ page-bio.php
@@ -7,11 +7,15 @@ get_header();
 
 <main id="main-content">
     <section class="page-content">
-        <?php
-        while ( have_posts() ) : the_post();
-            echo '<div class="bio-content">' . get_the_content() . '</div>';
-        endwhile;
-        ?>
+        <div class="bio-content">
+            <p>Hello! I'm Suzy Easton, a tech-savvy software QA engineer and former professional bassist, born and raised in Vancouver, BC. My journey began with piano lessons as a child, leading to performances at open mics as a teenager. After studying classical music and recording arts, I joined psychedelic rock band Seafoam, navigating through the punk scene and eventually playing bass for arena folk rock band, The Smokes/Minto. Our work took us across Canada and to recording in Chicago with the renowned Steve Albini.</p>
+            
+            <p>Parallel to my music career, I've carved out a successful path in technology, completely self-taught and driven by a strong passion for learning. After transitioning from music retail at HMV about 12 years ago, I delved into IT and software quality assurance. My journey has allowed me to work with top-tier companies like Electronic Arts, IBM, and ADP Canada, as well as in my current role as a Software QA Engineer at WineDirect. Across these roles, from system admin to project management and dev support, I've built a reputation for rapidly acquiring new skills and effectively applying them, all without any formal training in technology (unless you count music!).</p>
+            
+            <p>Outside of tech, my creative and entrepreneurial endeavours keep me busy. I'm currently working on a satirical documentary about my experiences as a single 41-year-old in Vancouver, blending humour with real-life scenarios. As a singer-songwriter, I'm not only composing new music but also gearing up for live performances soon. My passion for music continues with regular live video jam sessions, which I share online to connect with fans and fellow musicians. Moreover, I'm actively involved in creating new music videos that showcase my artistic vision.</p>
+            
+            <p>For collaborations, insights, or just to chat, feel free to reach out at <a href="mailto:suzyeaston@icloud.com">suzyeaston@icloud.com</a>. If you'd like to support my ongoing creative and technical endeavours, consider becoming a member on my <a href="https://www.patreon.com/SuzyEaston">Patreon</a> or buying me a coffee on <a href="https://www.buymeacoffee.com/wi0amge">Buy Me a Coffee</a>. Your support helps fuel my latest projects. Let's rock, code, and create together!</p>
+        </div>
     </section>
 </main>
 
