fbeb998d9ec162b210d27a5c0c0f4eb752ee4796
Suzy Easton <suzyeaston@icloud.com>
2025-02-23 03:15:13 -0800

    Update page-canucks-app.php

--- page-canucks-app.php
+++ page-canucks-app.php
@@ -7,23 +7,12 @@ get_header();
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
-
-        <!-- X (Twitter) Feed (may require you to be logged in, or for your account to be public) -->
-        <div class="canucks-twitter-feed">
-            <h2>Real-Time Canucks News</h2>
-            <a class="twitter-timeline"
-               href="https://twitter.com/officialsuzye?ref_src=twsrc%5Etfw">
-               Tweets from Suzy Easton, not sure if this will work lol
-            </a>
-            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-
-        <!-- Now the combined scoreboard + news feed -->
+        <!-- Display the combined Canucks App (News at top, followed by Schedule) -->
         <?php echo do_shortcode('[canucks_app]'); ?>
-
     </main><!-- #main -->
 </div><!-- #primary -->
 
 <?php get_footer(); ?>
+
 
 
