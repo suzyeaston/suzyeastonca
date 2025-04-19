4b0072d3d203d7dccdb68b57b077489d8d2cb766
Suzy Easton <suzyeaston@icloud.com>
2025-04-19 01:36:29 -0700

    Create page-albini-qa.php
    what would steve do? lol

---
+++ page-albini-qa.php
@@ -0,0 +1,8 @@
+<?php
+/* Template Name: Albini Q&A */
+get_header(); ?>
+  <main>
+    <h1>What Would Steve Albini suggest?</h1>
+    <?php echo do_shortcode('[albini_qa]'); ?>
+  </main>
+<?php get_footer(); ?>
