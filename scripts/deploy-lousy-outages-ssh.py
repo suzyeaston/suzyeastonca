63332209b8d67ecda9df21b4c314c13f5c903d84
Suzy Easton <suzyeaston@icloud.com>
2026-08-11 15:41:58 -0700

    Deploy homepage signal console stylesheet

--- scripts/deploy-lousy-outages-ssh.py
+++ scripts/deploy-lousy-outages-ssh.py
@@ -54,6 +54,7 @@ THEME_FILES = [
     "parts/shop-product-detail.php",
     "parts/lousy-outages-teaser.php",
     "parts/home-yvr-channel-buttons.php",
+    "assets/css/home-commercial-strip.css",
     "assets/css/shop.css",
     "assets/css/shop-console.css",
     "assets/js/shop.js",
